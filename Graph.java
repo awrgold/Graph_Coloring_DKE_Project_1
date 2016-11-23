44952d684e4cdec5afa60c3fe3b3a9c7743c286b
JurriaanBerger <JurriaanBerger@users.noreply.github.com>
2016-11-23 16:35:18 +0100

    Add files via upload
    Debugged the random graph generator (moreover some logical errors, of which the one that checks whether a vertex was already adjacent to that vertex is a major one). One other note: we need to decide how we count vertices and edges - do we start at 1 or 0? We are mixing them up at the moment: arrays start at 0, in this class vertices are printed as 1 if we mean vertex 1, while our algorithms of the previous phase use 0 for vertex one - which is way more logical. But we need to make a final decision and we all need to keep track of this!!!

--- Graph.java
+++ Graph.java
@@ -1,12 +1,13 @@
 import java.awt.Graphics;
-
+import java.util.Arrays;
 
 /**
-* V1.0 Created by Jurriaan Berger 
-* An object of this class contains the basic information about the graph and its features, i.e. edges and vertices.
-* Please keep following in mind: labelling of edges and vertices starts at 0!!!
-* This class is able to: draw graph, whether an x-y coordinate is inside a vertex,
-*/
+ * V1.2 Created by Jurriaan Berger edited by Jonas
+ * Random graph generator debugged by Jurriaan
+ * An object of this class contains the basic information about the graph and its features, i.e. edges and vertices.
+ * Please keep following in mind: labelling of edges and vertices starts at 0!!!
+ * This class is able to: draw graph, whether an x-y coordinate is inside a vertex,
+ */
 
 public class Graph{
 	//some explanation for that variable: it's gonna be n long, and contains in every row all neighbouring vertices with a higher index, the last one would thus have no entries
@@ -15,9 +16,9 @@ public class Graph{
 	private Vertex vertices[];
 	/**
 	 * Initialises a graph with given vertices and edges
-	 * @param vertices 
+	 * @param vertices
 	 * @param neighbours neighbours.length == vertices.length, every row contains the index of adjacent vertices with a higher index
-	 *  for more info look above 
+	 *  for more info look above
 	 */
 	public Graph(Vertex[] vertices, int[][] neighbours){
 		this.vertices = vertices;
@@ -28,20 +29,20 @@ public class Graph{
 	 * @param g a graphics object from a image/canvas to draw on
 	 */
 	public void draw(Graphics g){
-		//drawing the edges 
+		//drawing the edges
 		//one could add a if-statement so that the edges change color if any adjacent vertex is highlighted
 		//also some kind of style information could be passed on to the constructor and used here
 		//drawing the vertices
-				for(Vertex v : vertices){
-					v.draw(g);
-				}
+		for(Vertex v : vertices){
+			v.draw(g);
+		}
 		for(int i = 0;i<neighbours.length;i++)
 			for(int neighbour : neighbours[i])
-				g.drawLine(vertices[i].getX(), vertices[i].getY(), vertices[neighbour].getX(), vertices[neighbour].getY());
-		
+				g.drawLine(vertices[i].getX()+vertices[i].getDiameter()/2, vertices[i].getY()+vertices[i].getDiameter()/2, vertices[neighbour].getX()+vertices[neighbour].getDiameter()/2, vertices[neighbour].getY()+vertices[neighbour].getDiameter()/2);
+
 	}
 	/**
-	 * 
+	 *
 	 * @param x-coordinate of a point on the game canvas
 	 * @param y-coordinate of a point on the game canvas
 	 * @return -1 if no vertex could be found, otherwise the vertex's index is returned
@@ -54,7 +55,7 @@ public class Graph{
 		return -1;
 	}
 	/**
-	 * 
+	 *
 	 * @param v the index of a vertex (must be >= 0 and <n)
 	 * @return returns the vertex
 	 */
@@ -63,19 +64,19 @@ public class Graph{
 	}
 
 	/**
-	* Constructs a graph, that's loaded from a file
-	*/
+	 * Constructs a graph, that's loaded from a file
+	 */
 	//public static Graph readGraphFromFile(){
-		
-		//return new Graph
+
+	//return new Graph
 	//}
 
 	/** DEBUG ME, I NEVER GOT ACTUALLY TESTED, HELP ME D:
-	* Generates a random but connected graph
-	* @param n is the number of vertices, n>=2
-	* @param m is the number of edges, m is expected to be n-1<=m<=(n-1)*n*0.5 (the graph has to be at least connected and cannot contain more edges than a complete graph)
-	* @return Graph ....
-	*/
+	 * Generates a random but connected graph
+	 * @param n is the number of vertices, n>=2
+	 * @param m is the number of edges, m is expected to be n-1<=m<=(n-1)*n*0.5 (the graph has to be at least connected and cannot contain more edges than a complete graph)
+	 * @return Graph ....
+	 */
 	public static Graph generateRandomGraph(int n, int m){
 		//these arrays are going to be filled with the vertices and edges of the graph
 		Vertex[] vertices = new Vertex[n];
@@ -85,46 +86,42 @@ public class Graph{
 			int y = (int)  (Math.random()*((double) Game.HEIGHT));
 			vertices[i] = new Vertex(x,y);
 		}
-		//first, generate a path through the graph
-		for(int i = 1; i<n;i++){
-			neighbours[i-1][0] = i;
-		}
-		
-		//now randomly connected vertices that weren't connected before, until the desired number of edges is reached
-		//note: n-1 edges were used to create the path, so loop until m-(n-1)
-		for(int i=0; i<m-n+1; i++){
-			//randomly select two vertices to create a edge between them
-			int u = (int)(Math.random()*n);
-			int v = (int)(Math.random()*n);
-			//check for u==v
-			while(u==v){
-				v = (int)(Math.random()*n); //the random assigned vertex to v has to differ from u
-			}
-			boolean isSafeToAdd = true;
-			//check whether u and v are already connected
-			int min = Math.min(u, v);
-			int max = Math.max(u, v);
-			for(int j = 0; j<i;j++){
-				for(int k = 0;k<n-1;k++){
-					if(neighbours[min][k] == max){
-						isSafeToAdd = false;
-						i--;
-						break;
-					} else if(k>=1 && neighbours[min][k] == 0)
-						break;
+
+		//assigning neighbours to vertices for m times (== #edges)
+		for(int i = 0; i<m; i++){
+			//first, generate a path through the graph
+			if(i<n-1) neighbours[i][0] = i+2;//Actually the next vertex -> i+1 But this is due to issues on which place we start COUNTING
+				//now randomly connected vertices that weren't connected before, until the desired number of edges is reached
+			else {
+				//randomly select two vertices to create a edge between them
+				int u = 1+(int) (Math.random() * n);//COUNTING
+				int v = 1+(int) (Math.random() * n);//COUNTING
+				//check for u==v
+				while (u == v) {
+					v = 1+(int) (Math.random() * n); //the random assigned vertex to v has to differ from u COUNTING
 				}
-			}
-			if(isSafeToAdd){
-				//add to the first not used entry (as mentioned above the declaration of neighbours, 0 cannot occur naturally)
-				for(int j = 0;j<n;j++){
-					if(neighbours[min][j] == 0){
-						neighbours[min][j] = max;
+				//Check if the created edge already existed
+				for(int j=0; j<i; j++){
+					if((neighbours[u-1][j]==v)||(neighbours[v-1][j]==u)){ //COUNTING
+						i--; //Generate random vertices for edge[i] again
+						break;
+					}
+				}
+
+				int min = Math.min(u, v);
+				int max = Math.max(u, v);
+
+				// Store the new adjacent vertex at the right position
+				for (int j = 0; j < n; j++) {
+					if (neighbours[min-1][j] == 0) {
+						neighbours[min-1][j] = max;
 						break;
 					}
 				}
 			}
 		}
-		//now cut off the zeros (=unused entries) in every row of neighbours
+
+		//now cut off the zeros (=unused entries) in every row of neighbours COUNTING
 		for(int i = 0;i<n;i++){
 			for(int k = 0;k<m;k++){
 				if(neighbours[i][k] == 0){
@@ -135,6 +132,8 @@ public class Graph{
 				}
 			}
 		}
+
 		return new Graph(vertices, neighbours);
 	}
+
 }
