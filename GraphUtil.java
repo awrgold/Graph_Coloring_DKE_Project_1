6130b5e209a0a8513ec640cce40c61e96f3ba02a
JurriaanBerger <JurriaanBerger@users.noreply.github.com>
2016-12-16 00:43:33 +0100

    Update GraphUtil.java

--- GraphUtil.java
+++ GraphUtil.java
@@ -286,7 +286,7 @@ public class GraphUtil {
 		int distance; //the distance between all vertices
 		int cX = Game.WIDTH/2; //Define the center x-coordinate
 		int cY = Game.HEIGHT/2; //Define the center y-coordinate
-		final int MAX = 50
+		final int MAX = 50;
 		
 		//set the amount of circles
 		int sumA = MAX;
@@ -310,7 +310,7 @@ public class GraphUtil {
 		distance = radius/noOfCircles-Vertex.STANDARD_DIAMETER;
 		
 		//set the coordinates
-		int sumB = Math.min(max,n);
+		int sumB = Math.min(MAX,n);
 		int sumC = sumB;
 		int j=0;
 		
